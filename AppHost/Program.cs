16a662c950d3c1a86fdc43723cdd2f49d614a04c
Nils Quirmbach <nils.quirmbach@outlook.com>
2024-02-23 09:18:07 +0100

    Add direct reference to apphost from productservice

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -5,7 +5,18 @@ var postgres = builder.AddPostgresContainer("postgres");
 var redis = builder.AddRedisContainer("redis");
 
 // Microservices
-builder.AddProject("product-service", "../../ProductService/ProductService/ProductService.csproj")
+
+// Referencing a project out of the solution is not possible
+// Possible (but not really feasible) solution without referencing the project itself
+// https://github.com/dotnet/aspire/discussions/1137#discussioncomment-7713391
+
+// builder.AddProject("product-service", "../../ProductService/ProductService/ProductService.csproj")
+//     .WithReference(redis, "Redis");
+
+// Project needs to be referenced in AppHost project
+builder.AddProject<Projects.ProductService>("product-service")
     .WithReference(redis, "Redis");
- 
+
+// builder.AddExecutable()
+
 builder.Build().Run();
